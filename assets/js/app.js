43a24170068dc452f676ddc866993cbf64850e26
ashley-aguayo <ashley.aguayo@outlook.com>
2021-07-13 19:29:23 -0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,119 +1,97 @@
+// @TODO: YOUR CODE HERE!
+//Set Margins
 var svgWidth = 960;
-var svgHeight = 500;
+var svgHeight = 700;
 
 var margin = {
     top: 20,
     right: 40,
-    bottom: 60,
+    bottom: 100,
     left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+//create SVG wrapper
+
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("fill", "black")
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import Data
-d3.csv("data.csv").then(function(data) {
-
-    // Step 1: Parse Data/Cast as numbers
-    // ==============================
-    data.forEach(function(data) {
-      data.poverty = +data.poverty;
-      data.povertyMoe = +data.povertyMoe;
-      data.age = +data.age;
-      data.ageMoe = +data.ageMoe;
-      data.income = +data.incomeMoe;
-      data.healthcare = +data.healthcare;
-      data.healthcareLow = +data.healthcareLow;
-      data.healthcareHigh = +data.healthcareHigh;
-      data.obesity = +data.obesity;
-      data.obesityLow = +data.obesityLow;
-      data.obesityHigh = +data.obesityHigh;
-      data.smokes = +data.smokes;
-      data.smokesLow = +data.smokesLow;
-      data.smokesHigh = +data.smokesHigh;
+//Import data
+d3.csv("assets/data/data.csv").then(function(data){
+    console.log(data);
+    data.forEach(function(data){
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
     });
-
-    // Step 2: Create scale functions
-    // ==============================
+    
+    //create scales
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(data, d => d.state)])
-      .range([0, width]);
+    .domain([0, d3.max(data, d => d.poverty)])
+    .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(data, d => d.poverty)])
-      .range([height, 0]);
+    .domain([0, d3.max(data, d => d.healthcare)])
+    .range([height, 0]);
 
-    // Step 3: Create axis functions
-    // ==============================
+    //append axes 
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // Step 4: Append Axes to the chart
-    // ==============================
-    chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
-
-    chartGroup.append("g")
-      .call(leftAxis);
-
-    // Step 5: Create Circles
-    // ==============================
+    var xAxis = chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis)
+    var yAxis = chartGroup.append("g")
+    .call(leftAxis)
+    //create circles
     var circlesGroup = chartGroup.selectAll("circle")
     .data(data)
     .enter()
     .append("circle")
-    .attr("cx", d => xLinearScale(d.state))
+    .attr("cx", d => xLinearScale(d.healthcare))
     .attr("cy", d => yLinearScale(d.poverty))
     .attr("r", "15")
-    .attr("fill", "pink")
+    .attr("fill", "blue")
     .attr("opacity", ".5");
 
-    // Step 6: Initialize tool tip
-    // ==============================
+    //intialize tooltip
     var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (`${d.state}<br>Hair length: ${d.poverty}<br>Hits: ${d.income}`);
-      });
-
-    // Step 7: Create tooltip in the chart
-    // ==============================
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+    return (`${d.state}`);
+    });
+    //create and call tooltip
     chartGroup.call(toolTip);
 
-    // Step 8: Create event listeners to display and hide the tooltip
-    // ==============================
-    circlesGroup.on("click", function(data) {
-      toolTip.show(data, this);
+    //create event listener
+    circlesGroup.on("click", function(data){
+        toolTip.show(data,this);
     })
-      // onmouseout event
-      .on("mouseout", function(data, index) {
+    //mouseout
+    .on("mouseout", function(data,index){
         toolTip.hide(data);
-      });
+    });
 
-    // Create axes labels
+    //create labels for the axes
     chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Number of Billboard 100 Hits");
+    .attr("transform","rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height/2))
+    .attr("dy", "1em")
+    .text("Healthcare");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("Hair Metal Band Hair Length (inches)");
-  }).catch(function(error) {
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class","axisText")
+    .text("Poverty Percentage");
+}).catch(function(error){
     console.log(error);
-  });
+});
